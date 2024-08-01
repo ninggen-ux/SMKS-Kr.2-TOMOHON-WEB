8318af43ca8641b5f88904f1649ccd5da29d66f8
Pemerintah <tangkawarowandika@gmail.com>
2024-08-01 13:47:25 +0700

    update

--- script/sejarah.js
+++ script/sejarah.js
@@ -1,23 +1,11 @@
 const pimpinan = document.querySelector(".pimpinan");
-const angles = document.querySelectorAll(".angles");
+const leftAngles = document.querySelector(".fa-angles-left");
+const rightAngles = document.querySelector(".fa-angles-right");
 
-tombolKiriKanan = () => {
-    angles[0].style.opacity = 1;
-    angles[1].style.opacity = 1;
-}
-
-pimpinan.addEventListener("mouseover", tombolKiriKanan);
-pimpinan.addEventListener("scroll", tombolKiriKanan);
-
-pimpinan.addEventListener("mouseout", () => {
-    angles[0].style.opacity = 0;
-    angles[1].style.opacity = 0;
+leftAngles.addEventListener("click", () => {
+    pimpinan.scrollBy(-500, 0);
 });
 
-angles[0].addEventListener("click", () => {
-    pimpinan.scrollBy(-100, 0);
-});
-
-angles[1].addEventListener("click", () => {
-    pimpinan.scrollBy(100, 0);
+rightAngles.addEventListener("click", () => {
+    pimpinan.scrollBy(500, 0);
 });
